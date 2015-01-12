f72b9d37406dcc72044eb80cd2fcf6dcd58a91d3
Joost Kremers <joostkremers@fastmail.fm>
2015-01-12 13:30:20 +0100

    Add small comment to header block.

--- visual-fill-column.el
+++ visual-fill-column.el
@@ -26,7 +26,11 @@
 
 ;;; Commentary:
 
-;;
+;; `visual-fill-column-mode' is a small Emacs minor mode that mimics the
+;; effect of `fill-column' in `visual-line-mode'. Instead of wrapping lines
+;; at the window edge, which is the standard behaviour of
+;; `visual-line-mode', it wraps lines at `fill-column'. If `fill-column' is
+;; too large for the window, the text is wrapped at the window edge.
 
 ;;; Code:
 
