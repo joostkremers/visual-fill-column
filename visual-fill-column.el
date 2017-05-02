c0e5ec8f88a7598f7afdee565978410bee0de2a0
Nicholas D Steeves <nsteeves@gmail.com>
2017-05-02 15:28:35 -0400

    Fix potentially serious typos in license

--- visual-fill-column.el
+++ visual-fill-column.el
@@ -12,12 +12,12 @@
 
 ;; This file is NOT part of GNU Emacs.
 
-;; visual-fill-coulmn is free software: you can redistribute it and/or modify
+;; visual-fill-column is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; visual-fill-coulmn is distributed in the hope that it will be useful,
+;; visual-fill-column is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
