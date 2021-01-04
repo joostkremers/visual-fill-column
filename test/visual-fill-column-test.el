5b08b9ae9da5b95e42e94e4c9ec01d63e8848ea2
Joost Kremers <joostkremers@fastmail.fm>
2021-01-04 22:30:26 +0100

    Add test for `visual-fill-column--add-extra-width`.

--- test/visual-fill-column-test.el
+++ test/visual-fill-column-test.el
@@ -1,3 +1,15 @@
 ;;; visual-fill-column-test.el --- Tests for visual-fill-column
 
+;;; Commentary:
+;; Tests for the visual-fill-column package.
+
+;;; Code:
+
+(require 'visual-fill-column)
+
+(ert-deftest visual-fill-column--add-extra-width ()
+  (should (equal (visual-fill-column--add-extra-width 0  80 '(4 . 4)) '(0 . 76)))
+  (should (equal (visual-fill-column--add-extra-width 10 80 '(4 . 0)) '(6 . 80)))
+  (should (equal (visual-fill-column--add-extra-width 0  80 '(-4 . 0)) '(4 . 80))))
+
 ;;; visual-fill-column-test.el ends here
